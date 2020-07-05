42569432155afceb9dac75c784787786eb928bc8
Song Cho <deeplearner507@gmail.com>
2020-07-05 15:07:18 +0900

    add error boundary components

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Oh no! That is no no!!</h1>;
+    } else {
+      return this.props.children;
+    }
+  }
+}
+
+export default ErrorBoundary;
